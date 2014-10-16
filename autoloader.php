7dcd846ee80c5a41a032b71e642ea5392886118e
Ramesh Torres <RTorres4290@gmail.com>
2014-10-15 23:54:37 -0400

    Update autoloader.php

--- autoloader.php
+++ autoloader.php
@@ -1,6 +1,6 @@
 <?php
   spl_autoload(function($class) {
-    $filename='phpfile/' .$class. ' main.php';
+    $filename='phpfile/' .$class. ' .main.php';
 	if(is_readable($filename)){
 			require $filename;
 	}

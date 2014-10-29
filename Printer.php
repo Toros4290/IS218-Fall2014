920ef5249e2840f4dcbe71a71f5a7cd502632bf4
Ramesh Torres <RTorres4290@gmail.com>
2014-10-29 08:43:22 -0400

    Index

---
+++ Printer.php
@@ -0,0 +1,34 @@
+<?php
+namespace classes;
+
+class Printer
+{
+public static function printProfile(Array $record) {
+$profileVar = '<table class="table table-hover">';
+$index = -1;
+foreach($record as $records) {
+$index++;
+if($index === 0) {
+foreach($records['user'] as $key => $value) {
+$profileVar .= '<tr><th>' . ucfirst($key) . '</th><td>' . $value . '</td></tr>'; 
+}
+}
+}
+
+$profileVar .= '</table>';
+
+return $profileVar;
+} 
+
+public static function printTweets(Array $record) {
+$tweetVar = '<img src="' . $record[0]['user']['profile_image_url'] . '">';
+$tweetVar .= '<h1>Tweets by ' . $record[0]['user']['name'] . '</h1>';
+
+foreach($record as $records) {
+$tweetVar .= '<div class="row">' . $records['text'] . '</div>';
+}
+
+return $tweetVar;
+} 
+}
+?>
